dc23c3b38682287044327a86d74ab06e33b8d1ab
github username <altmanguitar@gmail.com>
2022-01-18 10:51:42 -0500

    commit tree 92d861193c801983523eaf8f083c21f54db36c51

--- src/components/RestaurantInput.js
+++ src/components/RestaurantInput.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { addRestaurant } from '../actions/restaurants';
 import { connect } from 'react-redux';
+import manageRestaurants from '../reducers/manageRestaurants';
 
 export class RestaurantInput extends Component {
 
@@ -23,7 +24,11 @@ export class RestaurantInput extends Component {
 
   handleOnSubmit = event => {
     event.preventDefault();
+    // debugger;
+
     // add missing code
+    console.log("Restaurant being added: ", this.state.restaurant);
+    this.props.addRestaurant(this.state)
   }
 
   render() {
@@ -49,6 +54,12 @@ export class RestaurantInput extends Component {
   }
 };
 
-
+const mapDispatchToProps = dispatch => {
+  return {
+    addRestaurant: (restaurant) => {
+      dispatch(addRestaurant(restaurant))
+    }
+  };
+};
 //connect this component by wrapping RestaurantInput below
-export default RestaurantInput
+export default connect(null, mapDispatchToProps)(RestaurantInput)
